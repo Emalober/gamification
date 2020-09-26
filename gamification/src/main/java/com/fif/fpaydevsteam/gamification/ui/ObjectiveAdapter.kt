9fa5dfa9d94a0f7d69ce8e5cdf2ebc92c61dca9b
Ezequiel Maloberti <emaloberti@technisys.com>
2020-09-26 07:31:02 -0300

    Se refina las cards

--- gamification/src/main/java/com/fif/fpaydevsteam/gamification/ui/ObjectiveAdapter.kt
+++ gamification/src/main/java/com/fif/fpaydevsteam/gamification/ui/ObjectiveAdapter.kt
@@ -3,13 +3,31 @@ package com.fif.fpaydevsteam.gamification.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.fif.fpaydevsteam.gamification.R
 import kotlinx.android.synthetic.main.item_progress_bar.view.*
 
+data class ObjectiveModel(val title: String, val progress: Int, val reward: String)
+
 class ObjectiveAdapter : RecyclerView.Adapter<ObjectiveAdapter.ObjectiveViewHolder>()  {
 
     inner class ObjectiveViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<ObjectiveModel>() {
+        override fun areItemsTheSame(oldItem: ObjectiveModel, newItem: ObjectiveModel): Boolean {
+            return oldItem.title == newItem.title
+        }
+
+        override fun areContentsTheSame(oldItem: ObjectiveModel, newItem: ObjectiveModel): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    fun submitList(list: List<ObjectiveModel>) = differ.submitList(list)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ObjectiveViewHolder {
         return ObjectiveViewHolder(
@@ -18,18 +36,18 @@ class ObjectiveAdapter : RecyclerView.Adapter<ObjectiveAdapter.ObjectiveViewHold
                 parent,
                 false
             )
-        )    }
+        )
+    }
 
     override fun getItemCount(): Int {
-        return 3
+        return differ.currentList.size
     }
 
     override fun onBindViewHolder(holder: ObjectiveViewHolder, position: Int) {
-        //val run = differ.currentList[position]
         holder.itemView.apply {
-            titleTextView.text = "Reto por definir"
-            progressBar.progress = 30
-            maxValueTextView.text = "500 P"
+            titleTextView.text = differ.currentList[position].title
+            progressBar.progress = differ.currentList[position].progress
+            maxValueTextView.text = differ.currentList[position].reward
         }
     }
 
--- gamification/src/main/java/com/fif/fpaydevsteam/gamification/ui/ProfileFragment.kt
+++ gamification/src/main/java/com/fif/fpaydevsteam/gamification/ui/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.fif.fpaydevsteam.gamification.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fif.fpaydevsteam.gamification.R
@@ -8,6 +10,18 @@ import kotlinx.android.synthetic.main.profile_gamification_fragment.*
 class ProfileFragment : Fragment(R.layout.profile_gamification_fragment)  {
 
     private lateinit var objAdapter: ObjectiveAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecycleView()
+
+        val logros = listOf(ObjectiveModel("Pagar 5 veces con Fpay", 20, "200 Exp"),
+                            ObjectiveModel("Referir a 5 amigos", 60, "500 Exp"),
+                            ObjectiveModel("Visita la nueva tienda", 0, "200 P.CMR"))
+
+        objAdapter.submitList(logros)
+    }
 
     private fun setupRecycleView() = objectivesList.apply {
         objAdapter = ObjectiveAdapter()
